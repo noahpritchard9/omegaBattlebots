dfff6778bb9ed525522abafc00021bb4748adf96
Noah Pritchard <noahpritchard9@gmail.com>
2022-11-12 13:05:58 -0500

    set up google maps api

--- appDev/apps/expo/src/screens/map.tsx
+++ appDev/apps/expo/src/screens/map.tsx
@@ -1,33 +1,86 @@
-import { View, Text, StyleSheet, Dimensions } from 'react-native'
-import MapView, { Polyline } from 'react-native-maps'
+import { useRef, useState } from 'react'
+import { Dimensions, SafeAreaView, StyleSheet, View, Text } from 'react-native'
+import MapView, { LatLng, PROVIDER_GOOGLE, Region } from 'react-native-maps'
+import MapViewDirections from 'react-native-maps-directions'
 
-export const Map = ({ navigation }: { navigation: unknown }) => {
+const { width, height } = Dimensions.get('window')
+
+const ASPECT_RATIO = width / height
+
+const LATITUDE_DELTA = 0.02
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO
+const INITIAL_REGION = {
+	latitude: 38.9007,
+	longitude: 77.0518,
+	latitudeDelta: LATITUDE_DELTA,
+	longitudeDelta: LONGITUDE_DELTA,
+}
+
+const origin: LatLng = { latitude: 37.79879, longitude: -122.442753 }
+
+const dest: LatLng = { latitude: 37.790651, longitude: -122.422497 }
+
+const GOOGLE_MAPS_API_KEY = 'AIzaSyCTW63C0yWF4hxbX4dovYZJNzRwyw_W_Ss'
+
+export const Map = (navigation: { navigation: any }) => {
+	const mapRef = useRef<MapView>(null)
+
+	const [region, setRegion] = useState<Region>(INITIAL_REGION)
+
+	const [duration, setDuration] = useState<string>('')
+	const [distance, setDistance] = useState<string>('')
+
 	return (
-		<View className='flex items-center justify-center'>
+		<SafeAreaView className='flex items-center justify-center'>
 			<MapView
+				ref={mapRef}
+				provider={PROVIDER_GOOGLE}
 				style={styles.map}
-				initialRegion={{
-					latitude: 38.905869,
-					longitude: -77.051598,
-					latitudeDelta: 0.0922,
-					longitudeDelta: 0.0421,
-				}}
-				showsUserLocation
+				initialRegion={region}
+				// region={region}
+				onRegionChange={r => setRegion(r)}
+				// region={INITIAL_REGION}
+				// onRegionChangeComplete={() => {
+				// 	mapRef.current?.animateToRegion(INITIAL_REGION)
+				// }}
 			>
-				<Polyline
-					coordinates={[
-						{ latitude: 38.889484, longitude: -77.035278 },
-						{ latitude: 38.897675, longitude: -77.03653 },
-					]}
-				/>
+				<MapViewDirections
+					origin={origin}
+					destination={dest}
+					apikey={GOOGLE_MAPS_API_KEY}
+					strokeWidth={3}
+					strokeColor='hotpink'
+					onReady={res => {
+						setDuration(res.duration.toFixed(0))
+						setDistance(res.distance.toFixed(1))
+						mapRef.current?.fitToCoordinates(res.coordinates, {
+							edgePadding: {
+								top: height / 20,
+								right: width / 20,
+								bottom: height / 20,
+								left: width / 20,
+							},
+						})
+					}}
+				></MapViewDirections>
 			</MapView>
-		</View>
+			{duration && (
+				<Text className='absolute top-0 right-0 m-2 font-bold'>
+					{duration} minutes
+				</Text>
+			)}
+			{distance && (
+				<Text className='absolute top-4 right-0 m-2 font-bold'>
+					{distance} km
+				</Text>
+			)}
+		</SafeAreaView>
 	)
 }
 
 const styles = StyleSheet.create({
 	map: {
-		...StyleSheet.absoluteFillObject,
+		// flex: 1,
 		height: Dimensions.get('window').height,
 		width: Dimensions.get('window').width,
 	},
