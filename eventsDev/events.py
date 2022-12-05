ef8c854c6241b3de6734ea85dbbb0743f7fb8d5a
Luisa Ball <luisaball@Luisas-MacBook-Pro.local>
2022-12-04 19:38:59 -0500

    fixed events details

--- eventsDev/events.py
+++ eventsDev/events.py
@@ -6,15 +6,20 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from datetime import date
+
+today = date.today()
+year = today.strftime("%Y")
+month = today.strftime("%m")
+day = today.strftime("%d")
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("headless")
 driver = webdriver.Chrome(options=chrome_options)
 
-#Will replace the url 
-url = 'https://washington.org/find-dc-listings/events?start_date_value=11%2F29%2F2022&end_date_value='
-driver.get(url)
-# driver.implicitly_wait(10)
+url = 'https://washington.org/find-dc-listings/events?start_date_value={m}%2F{d}%2F{y}&end_date_value='
+
+driver.get(url.format(y=year, m = month, d = day))
 
 elem = driver.find_elements(By.PARTIAL_LINK_TEXT,"VIEW DETAILS")
 
@@ -29,21 +34,13 @@ eventsList = ""
 des= []
 for i in url_list:
     driver = webdriver.Chrome(options=chrome_options)
-    # print(i)
     driver.get(i)
     eventName = driver.find_element(By.CLASS_NAME,'neighborhood-hero__banner__label.neighborhood-hero__banner__label--heading.label--blaze').text
     eventDate = driver.find_element(By.CLASS_NAME, "deal-validity").text
-    # print(eventDate.text)
     address1 = driver.find_elements(By.CLASS_NAME,"address")[1].text
     
     description = driver.find_element(By.CLASS_NAME, "text-long").text
-    # local = driver.find_element(By.CLASS_NAME, "deal-validity").text
-    # area = driver.find_element(By.CLASS_NAME, "administrative-area")
-    # zipcode = driver.find_elements(By.CLASS_NAME, "postal-code")[1].text
-    
-    for i in description:
-        des.append(i.replace("\n", ""))
-    # description.replace('\n','')
+
     driver.close()
     
     
@@ -64,7 +61,7 @@ json =  '''{
     ]
 }''' % (url, eventsList)
 
-with open('eventsData.json', 'w') as page:
+with open('eventsDev/eventsData.json', 'w') as page:
     page.write(json)
     page.close()
     
