fa471ee367e38b036249a0e149577ed436cd78d8
Noah Pritchard <noahpritchard9@gmail.com>
2022-12-15 00:03:31 -0500

    add new writing to website/change website design

--- appDev/apps/expo/src/screens/map.tsx
+++ appDev/apps/expo/src/screens/map.tsx
@@ -1,60 +1,65 @@
-import { useQuery } from '@tanstack/react-query'
-import { useRef, useState } from 'react'
-import { Dimensions, SafeAreaView, StyleSheet, View, Text } from 'react-native'
-import MapView, { LatLng, PROVIDER_GOOGLE, Region } from 'react-native-maps'
-import MapViewDirections from 'react-native-maps-directions'
+import { useQuery } from '@tanstack/react-query';
+import { useRef, useState } from 'react';
+import { Dimensions, SafeAreaView, StyleSheet, View, Text } from 'react-native';
+import MapView, { LatLng, PROVIDER_GOOGLE, Region } from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
 
-import { GOOGLE_MAPS_API_KEY } from '../apiKeys'
+import { GOOGLE_MAPS_API_KEY } from '../apiKeys';
 
 export interface GoogleLocationResponse {
-	location: Location
-	accuracy: number
+	location: Location;
+	accuracy: number;
 }
 
 export interface Location {
-	lat: number
-	lng: number
+	lat: number;
+	lng: number;
 }
 
-const { width, height } = Dimensions.get('window')
+const { width, height } = Dimensions.get('window');
 
-const ASPECT_RATIO = width / height
+const ASPECT_RATIO = width / height;
 
-const LATITUDE_DELTA = 0.02
-const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO
+const LATITUDE_DELTA = 0.02;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 const INITIAL_REGION = {
 	latitude: 38.9007,
 	longitude: 77.0518,
 	latitudeDelta: LATITUDE_DELTA,
 	longitudeDelta: LONGITUDE_DELTA,
-}
+};
 
 export const Map = (navigation: { navigation: any }) => {
-	const mapRef = useRef<MapView>(null)
+	const mapRef = useRef<MapView>(null);
 
-	const [region, setRegion] = useState<Region>(INITIAL_REGION)
+	const [region, setRegion] = useState<Region>(INITIAL_REGION);
 
-	const [duration, setDuration] = useState<string>('')
-	const [distance, setDistance] = useState<string>('')
+	const [duration, setDuration] = useState<string>('');
+	const [distance, setDistance] = useState<string>('');
 
-	const fetchLocation = async (): Promise<GoogleLocationResponse> =>
-		(
-			await fetch(
-				`https://www.googleapis.com/geolocation/v1/geolocate?key=${GOOGLE_MAPS_API_KEY}`
-			)
-		).json()
+	// const fetchLocation = async (): Promise<GoogleLocationResponse> =>
+	// 	(
+	// 		await fetch(
+	// 			`https://www.googleapis.com/geolocation/v1/geolocate?key=${GOOGLE_MAPS_API_KEY}`
+	// 		)
+	// 	).json()
 
-	const location = useQuery({
-		queryKey: ['userLocation'],
-		queryFn: fetchLocation,
-	})
+	// const location = useQuery({
+	// 	queryKey: ['userLocation'],
+	// 	queryFn: fetchLocation,
+	// })
+
+	// const origin: LatLng = {
+	// 	latitude: location.data?.location.lat ?? 37.79879,
+	// 	longitude: location.data?.location.lng ?? -122.442753,
+	// }
 
 	const origin: LatLng = {
-		latitude: location.data?.location.lat ?? 37.79879,
-		longitude: location.data?.location.lng ?? -122.442753,
-	}
+		latitude: 37.79879,
+		longitude: -122.442753,
+	};
 
-	const dest: LatLng = { latitude: 37.790651, longitude: -122.422497 }
+	const dest: LatLng = { latitude: 37.790651, longitude: -122.422497 };
 
 	return (
 		<SafeAreaView className='flex items-center justify-center'>
@@ -76,8 +81,8 @@ export const Map = (navigation: { navigation: any }) => {
 						longitude: e.nativeEvent.coordinate?.longitude!,
 						latitudeDelta: 0.01,
 						longitudeDelta: 0.01,
-					})
-					mapRef.current?.animateToRegion(region)
+					});
+					mapRef.current?.animateToRegion(region);
 				}}
 			>
 				<MapViewDirections
@@ -87,8 +92,8 @@ export const Map = (navigation: { navigation: any }) => {
 					strokeWidth={3}
 					strokeColor='hotpink'
 					onReady={res => {
-						setDuration(res.duration.toFixed(0))
-						setDistance(res.distance.toFixed(1))
+						setDuration(res.duration.toFixed(0));
+						setDistance(res.distance.toFixed(1));
 						mapRef.current?.fitToCoordinates(res.coordinates, {
 							edgePadding: {
 								top: height / 20,
@@ -96,7 +101,7 @@ export const Map = (navigation: { navigation: any }) => {
 								bottom: height / 20,
 								left: width / 20,
 							},
-						})
+						});
 					}}
 				></MapViewDirections>
 			</MapView>
@@ -111,8 +116,8 @@ export const Map = (navigation: { navigation: any }) => {
 				</Text>
 			)}
 		</SafeAreaView>
-	)
-}
+	);
+};
 
 const styles = StyleSheet.create({
 	map: {
@@ -120,4 +125,4 @@ const styles = StyleSheet.create({
 		height: Dimensions.get('window').height,
 		width: Dimensions.get('window').width,
 	},
-})
+});
