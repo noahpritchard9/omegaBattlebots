29bf7ead71f9401ef5f89edb49181fab098c7797
Luisa Ball <luisaball@Luisas-MacBook-Pro.local>
2022-12-14 23:27:47 -0500

    final

--- shadeDev/buildingsCalc.py
+++ shadeDev/buildingsCalc.py
@@ -10,35 +10,47 @@ from geopy.distance import distance, Distance
 from geopy.distance import lonlat, distance
 from geopy.point import Point
 import math
+import time
 import geopy
+from shadeCalc import shadowCalc
+from webdriver_manager.chrome import ChromeDriverManager
 
+class buildings():
+    def __init__(self,path_lat, path_long):
+        self.path_lat = path_lat
+        self.path_long = path_long
+    def shade(self,path_lat, path_long):
+        #get azimuth (angle) 
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("headless")
+        driver = webdriver.Chrome(options=chrome_options)
+        # driver = webdriver.Chrome(ChromeDriverManager().install())
+        url = 'https://www.suncalc.org/#/{la},{lo},17/null/08:30/130/2'
 
-path_lat = 38.90050
-path_long =-77.04670
+        driver.get(url.format(la=path_lat, lo = path_long))
+        # time.sleep(15)
+        elem = driver.find_element(By.ID, "azimuth").text
+        print("Shade angle:",elem)
+        driver.close()
 
-#get azimuth (angle)
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("headless")
-driver = webdriver.Chrome(options=chrome_options)
-url = 'https://www.suncalc.org/#/{la},{lo},17/null/08:30/130/2'
+        bear = float(elem.replace('°', ''))
 
-driver.get(url.format(la=path_lat, lo = path_long))
+        print("Path point:", path_lat, path_long )
 
-elem = driver.find_element(By.ID, "azimuth").text
-print("Azimuth:",elem)
-driver.close()
-
-bear = float(elem.replace('°', ''))
-
-
-pt = distance(meters=50).destination((path_lat,path_long), bearing=bear)
-    
-print("Building point:", pt[0], pt[1])
-print("Path point:", path_lat, path_long )
-
-
-        
-        
-    
+        count = 1
+        # iterate through buildings down the line of the shadow
+        for i in range(10,130,15):
+            
+            print("\nBuilding #",count, "")
+            print("--------------")
+            pt = distance(meters=i).destination((path_lat,path_long), bearing=bear)
+            print("Building point:", pt[0], pt[1])
+            shadow = shadowCalc(path_lat, path_long, pt[0], pt[1])
+            shadeId = shadow.shade(path_lat, path_long, pt[0], pt[1]) 
+            if shadeId == 1:
+                break
+            count=count+1
+            
+                
 
 
--- shadeDev/shadeCalc.py
+++ shadeDev/shadeCalc.py
@@ -2,38 +2,100 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 import math
+import time
 
-#numbers imported from points script
-path_lat = 38.89046
-path_long = -77.03211
-build_lat = 38.89035
-build_long = -77.03195
-build_height = 130
+class shadowCalc():
+    def __init__(self, path_lat, path_long, build_lat, build_long):
+        self.path_lat = path_lat
+        self.path_long = path_long
+        self.build_lat = build_lat
+        self.build_long = build_long
 
-#Harversine formula to get distance between the two points
-path_lat = math.radians(path_lat)
-path_long = math.radians(path_long)
-build_lat = math.radians(build_lat)
-build_long = math.radians(build_long)
+    def shade(self, path_lat, path_long, build_lat, build_long):
+        
+        #will get height from elevation map 
+        build_height = shadowCalc.height(self, build_lat,build_long)
+        
+        #Harversine Formula to get distance between the two points
+        path_lat2 = math.radians(path_lat)
+        path_long2 = math.radians(path_long)
+        build_lat2 = math.radians(build_lat)
+        build_long2 = math.radians(build_long)
 
-num = math.sin((build_lat - path_lat) / 2)**2 + math.cos(path_lat) * math.cos(build_lat) * math.sin((build_long - path_long) / 2)**2
-sq_num = math.sqrt(num)
-distance = 2 * math.asin(sq_num) * 6371000
-print(distance)
+        num = math.sin((build_lat2 - path_lat2) / 2)**2 + math.cos(path_lat2) * math.cos(build_lat2) * math.sin((build_long2 - path_long2) / 2)**2
+        sq_num = math.sqrt(num)
+        distance = 2 * math.asin(sq_num) * 6371000
+        print("distance path-building:", distance)
 
-#get shadow length (m)
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("headless")
-driver = webdriver.Chrome(options=chrome_options)
-url = 'https://www.suncalc.org/#/{la},{lo},17/2022.12.04/08:30/{h}/2'
+        #get shadow length (m)
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("headless")
+        driver = webdriver.Chrome(options=chrome_options)
+        # driver = webdriver.Chrome(ChromeDriverManager().install())
 
-driver.get(url.format(la=build_lat, lo = build_long, h = build_height))
+        url = 'https://www.suncalc.org/#/{la},{lo},17/null/09:30/{h}/2'
+        driver.get(url.format(la=build_lat, lo = build_long, h = build_height))
+        # time.sleep(30)
 
-elem = float(driver.find_element(By.ID, "schatten").text)
-print(elem)
-driver.close()
+        elem = float(driver.find_element(By.ID, "schatten").text)
+        print("shadow length:",elem)
+        driver.close()
 
-#does building shadow reach path point?
-if elem > distance:
-    print("yes")
+        #does building shadow reach path point?
+        if elem > distance:
+            print("Shady path?: YES")
+            return 1
+        else:
+            print("Shady path?: NO")
+    
+    def height(self, build_lat, build_long):
+        if build_lat == 38.900595794515404 and build_long == -77.04650672345407:
+            return 39.23
+        if build_lat == 38.899460294503704 and build_long == -77.04651482482335:
+            return 25.98
+        if build_lat == 38.899102660183694 and build_long == -77.04518617678218:
+            return 28.17
+        
+        if build_lat == 38.8993785861396 and build_long == -77.04460756230469:
+            return 50.41
+        else: 
+            return 0
+        
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#input 3/4 lat long sidewalk lat long
+#need building height, lat and long
+# 130 feet (39.624) (commercial streets) and 
+# 90 feet (27.432) (residential streets), and 
+# 160 feet (48.768)for parts of Pennsylvania Avenue, NW.
+# if between is N
+# so check buldings north
+#  else
+#  
+# 
+# what if we get azimuth depending on time and check buildings down that line . 
+#import function create object
+# show suncalc
+# 1 connect scripts
+# work back
+# height
+# writing2
+# create method shade calc and call inside for loop``.
+# ask about height
+
